aa3f0b2ea19a881a8feaeb892f885c060faf3d60
BaGorK <edmealemkassahun@gmail.com>
2024-05-17 00:59:00 +0300

    feat: setup a basic express app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('HELLO FROM THE SERVER');
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+try {
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}...`));
+} catch (error) {
+  console.error('📛', error);
+  process.exit(1);
+}
