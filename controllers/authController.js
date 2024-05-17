499634cfb4310efd7f90e5885888ce260a07c16a
BaGorK <edmealemkassahun@gmail.com>
2024-05-17 03:17:14 +0300

    feat: user roles | set first user as admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,11 +3,20 @@ import User from '../models/userModel.js';
 import BadRequestError from '../errors/bad-request.js';
 
 export const register = async (req, res, next) => {
-  const { email } = req.body;
-  if (User.findOne({ email })) {
-    throw BadRequestError('Email already exists');
+  const { name, email, password } = req.body;
+
+  const isFirstUser = (await User.countDocuments()) === 0;
+  const role = isFirstUser ? 'admin' : user;
+
+  if (await User.findOne({ email })) {
+    throw new BadRequestError('Email already exists');
   }
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
 
   return res.status(StatusCodes.CREATED).json({
     status: 'success',
