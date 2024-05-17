3c04a2b8a35318ac25d63673149c595a1c925a8a
BaGorK <edmealemkassahun@gmail.com>
2024-05-17 06:26:23 +0300

    feat: implement login route controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import BadRequestError from '../errors/bad-request.js';
 import { attachCookiesToResponse, createJWT } from '../utils/tokenUtils.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
 
 export const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -38,8 +39,27 @@ export const register = async (req, res, next) => {
   });
 };
 
-export const login = (req, res, next) => {
-  return res.status(200).json({ message: 'login' });
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new BadRequestError('Please the correct credentials');
+
+  const user = await User.findOne({ email });
+  if (!user) throw new UnauthenticatedError('Invalid Credentials');
+
+  if (!(await user.comparePassword(password)))
+    throw new BadRequestError('Invalid Credentials');
+
+  attachCookiesToResponse({
+    res,
+    user: { name: user.name, userId: user._id, role: user.role },
+  });
+
+  return res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    message: 'login successful',
+    data: { user },
+  });
 };
 
 export const logout = (req, res, next) => {
