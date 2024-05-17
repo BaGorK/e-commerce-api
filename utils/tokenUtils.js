eee0a819247fd35f25a5d298533aa25dd5027db8
BaGorK <edmealemkassahun@gmail.com>
2024-05-17 10:48:55 +0300

    feat: refactor token utils

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,14 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-export const createJWT = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET, {
+export const createJWT = (user) =>
+  jwt.sign(user, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-export const verifyJWT = (token) => jwt.verify(token, process.env.JWT_SECRET);
+export const verifyJWT = ({ token }) =>
+  jwt.verify(token, process.env.JWT_SECRET);
 
 export const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+  const token = createJWT(user);
 
   res.cookie('token', token, {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
