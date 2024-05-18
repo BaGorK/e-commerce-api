b4284b3999f263a30699c6d53b73b69cdbf1e929
BaGorK <edmealemkassahun@gmail.com>
2024-05-18 22:54:31 +0300

    feat: add timestamps on review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,21 +1,26 @@
 import mongoose from 'mongoose';
 
-const ReviewSchema = new mongoose({
-  title: {
-    type: String,
+const ReviewSchema = new mongoose(
+  {
+    title: {
+      type: String,
+    },
+    comment: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
   },
-  comment: {
-    type: String,
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-  },
-  product: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Product',
-  },
-});
+  { timestamps: true }
+);
 
 const Review = mongoose.model('Review', ReviewSchema);
 
