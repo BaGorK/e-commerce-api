5726787a9ed6cfd2596674a994feaebf6bd96dfd
BaGorK <edmealemkassahun@gmail.com>
2024-05-18 19:35:03 +0300

    feat: add a time stamp to a product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,44 +1,49 @@
 import mongoose from 'mongoose';
 
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'product name is required'],
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'product name is required'],
+    },
+    price: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    company: {
+      type: String,
+    },
+    colors: {
+      type: [],
+    },
+    featured: {
+      type: Boolean,
+    },
+    freeShipping: {
+      type: Number,
+    },
+    inventory: {
+      type: Number,
+    },
+    averageRating: {
+      type: Number,
+    },
+    user: {
+      type: String,
+    },
   },
-  price: {
-    type: Number,
-  },
-  description: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  category: {
-    type: String,
-  },
-  company: {
-    type: String,
-  },
-  colors: {
-    type: [],
-  },
-  featured: {
-    type: Boolean,
-  },
-  freeShipping: {
-    type: Number,
-  },
-  inventory: {
-    type: Number,
-  },
-  averageRating: {
-    type: Number,
-  },
-  user: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model('Product', ProductSchema);
 
