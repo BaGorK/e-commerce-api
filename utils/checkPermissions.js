ea209a89151c8fcacf3be5eb20f678956f997c72
BaGorK <edmealemkassahun@gmail.com>
2024-05-18 19:24:46 +0300

    feat: implement checkPermissions on get single user fn

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,7 +1,17 @@
+import UnauthenticatedError from '../errors/unauthenticated.js';
+import UnauthorizedError from '../errors/unauthorized.js';
+
 const checkPermissions = (requestUser, resourceUserId) => {
-  console.log(requestUser);
-  console.log(resourceUserId);
-  console.log(typeof resourceUserId);
+  // console.log(requestUser);
+  // console.log(resourceUserId);
+  // console.log(typeof resourceUserId);
+  // { name: 'test', userId: '6648d2c201ce0504dfa59800', role: 'user' }
+  // new ObjectId('6648d2c801ce0504dfa59804')
+
+  if (requestUser.role === 'admin') return;
+  if (requestUser.userId === resourceUserId.toString()) return;
+
+  throw new UnauthorizedError('Not authorized to access this route');
 };
 
 export default checkPermissions;
