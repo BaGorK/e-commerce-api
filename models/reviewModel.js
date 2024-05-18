a944bab8305548ea5b17b704f93e017a7f1273b1
BaGorK <edmealemkassahun@gmail.com>
2024-05-18 22:59:08 +0300

    feat: update review schema

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -4,9 +4,19 @@ const ReviewSchema = new mongoose(
   {
     title: {
       type: String,
+      trim: true,
+      required: [true, 'please provide review title'],
+      maxlength: 100,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
     },
     comment: {
       type: String,
+      required: [true, 'Please provide review text'],
     },
     user: {
       type: mongoose.Types.ObjectId,
