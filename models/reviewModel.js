64ef16865624ce7efb69c76db5f0f7f903ebc1a1
BaGorK <edmealemkassahun@gmail.com>
2024-05-24 21:03:23 +0300

    feat: review model aggregation pipeline implemented

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,7 +34,29 @@ const ReviewSchema = new mongoose.Schema(
 
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
-ReviewSchema.statics.calcAverageRating = async function (productId) {};
+ReviewSchema.statics.calcAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  console.log(result);
+
+  try {
+    await this.model('Product').findByIdAndUpdate(productId, {
+      averageRating: Math.round(result[0]?.averageRating * 10) / 10 || 0,
+      numOfReviews: result[0]?.numOfReviews || 0,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 ReviewSchema.post('save', async function () {
   await this.constructor.calcAverageRating(this.product);
