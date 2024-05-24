b5023348681983db45e6d827c68c4fd4c604c24d
BaGorK <edmealemkassahun@gmail.com>
2024-05-24 22:22:25 +0300

    fix: typos on order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -12,47 +12,49 @@ const SingleCartItemSchema = mongoose.Schema({
   },
 });
 
-const OrderSchema = new mongoose.Schema({
-  tax: {
-    type: Number,
-    required: true,
-    _,
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItem: [SingleCartItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+      required: true,
+    },
   },
-  shippingFee: {
-    type: Number,
-    required: true,
-  },
-  subtotal: {
-    type: Number,
-    required: true,
-  },
-  total: {
-    type: Number,
-    required: true,
-  },
-  cartItem: [SingleCartItemSchema],
-  status: {
-    type: String,
-    enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
-    default: 'pending',
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  clientSecret: {
-    type: String,
-    required: true,
-  },
-  paymentIntentId: {
-    type: String,
-    required: true,
-  },
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Order = mongoose.deleteModel('Order', OrderSchema);
+const Order = mongoose.model('Order', OrderSchema);
 
 export default Order;
