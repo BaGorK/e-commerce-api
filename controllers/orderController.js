8578cea50df6c2661d198a6f29a8bb5e29ddcad0
BaGorK <edmealemkassahun@gmail.com>
2024-05-24 22:26:27 +0300

    feat: check  before create order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,26 @@
 import { StatusCodes } from 'http-status-codes';
+import Order from '../models/orderModel.js';
+import Product from '../models/productModel.js';
+import BadRequestError from '../errors/bad-request.js';
+import NotFoundError from '../errors/not-found.js';
+import checkPermissions from '../utils/checkPermissions.js';
+
+export const createOrder = async (req, res) => {
+  const { item: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new BadRequestError('No cart items provided');
+  }
+
+  if (!tax || !shippingFee) {
+    throw new BadRequestError('please provide tax and shipping fee');
+  }
+
+  return res.status(StatusCodes.OK).json({
+    status: 'success',
+    message: 'createOrder',
+  });
+};
 
 export const getAllOrders = async (req, res) => {
   return res.status(StatusCodes.OK).json({
@@ -20,14 +42,6 @@ export const getCurrentUserOrders = async (req, res) => {
     message: 'getCurrentUserOrders',
   });
 };
-
-export const createOrder = async (req, res) => {
-  return res.status(StatusCodes.OK).json({
-    status: 'success',
-    message: 'createOrder',
-  });
-};
-
 export const updateOrder = async (req, res) => {
   return res.status(StatusCodes.OK).json({
     status: 'success',
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   createOrder,
   getAllOrders,
+  getCurrentUserOrders,
   getSingleOder,
   updateOrder,
 } from '../controllers/orderController.js';
@@ -17,6 +18,8 @@ Router.use(authenticateUser);
 Router.route('/')
   .get(authorizePermissions('admin'), getAllOrders)
   .post(createOrder);
+
+Router.route('/showAllMyOrders').get(getCurrentUserOrders);
 
 Router.route('/:id').patch(updateOrder).get(getSingleOder);
 
