d8f676074d33e314849290baba3601f13443d13c
BaGorK <edmealemkassahun@gmail.com>
2024-05-24 22:38:58 +0300

    feat: creat order | create singleOrderItem

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -16,6 +16,36 @@ export const createOrder = async (req, res) => {
     throw new BadRequestError('please provide tax and shipping fee');
   }
 
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.Product });
+
+    if (!dbProduct) {
+      throw new NotFoundError(`No Product with id: ${item.Product} found`);
+    }
+
+    const { name, price, image, _id } = dbProduct;
+
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+
+    // add items to order
+    orderItems += item.amount * price;
+
+    // calculate subtotal
+    subtotal += item.amount * price;
+  }
+
+  console.log(orderItems);
+  console.log(subtotal);
+
   return res.status(StatusCodes.OK).json({
     status: 'success',
     message: 'createOrder',
